fcb89eae7064f0815aa0e75b7d547303360204ce
Hel Nershing Thapa <helnershingthapa@gmail.com>
2021-10-03 16:35:37 +0545

    static map viz desc added

--- app/components/MapInsightsView/StaticMaps.js
+++ app/components/MapInsightsView/StaticMaps.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import { uid } from 'react-uid';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
@@ -22,20 +23,14 @@ import ZoomImage from 'components/StoryDetailView/ZoomImage';
 
 const staticMaps = [
   {
-    fileName: 'map-insight-1.png',
-    title: 'Distribution of respondents by occupation and district',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
-  },
-  {
-    fileName: 'migration-pokhara.png',
-    title: 'Flow of Tourism Workers to Pokhara with Different Skills',
+    fileName: 'tourism-destinations.png',
+    title: 'Major Tourism Destinations',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
   },
   {
     fileName: 'migration-kathmandu.png',
-    title: 'Flow of Tourism Workers to Pokhara with Different Skills',
+    title: 'Flow of Tourism Workers to Kathmandu with Different Skills',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
   },
@@ -47,30 +42,105 @@ const staticMaps = [
   },
   {
     fileName: 'geo-distribution.png',
-    title: 'Geographical Distribution of TOurism Workers by Their Occupation',
+    title: 'Geographical Distribution of Tourism Workers by Their Occupation',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
-  },
-  {
-    fileName: 'perma-addr-distribution.png',
-    title: 'Distribution of Tourism Workers by Their Permanent Address ',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
-  },
-  {
-    fileName: 'tourism-destinations.png',
-    title: 'Major Tourism Destinations',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum.',
+      'This map is a standard univariate product that shows district-wise distribution of our respondents, or how reported tourism occupations vary across districts. Turns out 20% of our respondents are from Solukhumbu district, where the highest peak of the world, Mount Everest is located. Most of the respondents here are Mountain and trekking guides.',
   },
 ];
 
 function StaticMaps() {
+  const getDescription = index => {
+    switch (index) {
+      case 1: {
+        return (
+          <>
+            <Typography paragraph>
+              this map identifies the major tourism hubs and destinations of the
+              country by looking into the movement of the respondents during
+              their work.
+            </Typography>
+            <Typography paragraph>
+              We prepared this map by combining the responses from the survey
+              with the existing OSM data and checking the validity of the hubs.
+            </Typography>
+            <Typography paragraph>
+              The areas with diagonal lines show the major tourism hubs. We
+              figured the major tourism hubs were Kathmandu, the capital city of
+              Nepal that houses the only international airport in Nepal.
+              Pokhara, which has the route to Annapurna trekking. The area is
+              also famous for adventure sports. Next, we have Chitwan - the
+              gateway to national parks.
+            </Typography>
+          </>
+        );
+      }
+      case 2: {
+        return (
+          <>
+            <Typography paragraph>
+              This map depicts the nature and distribution of tourism skills
+              that Kathmandu draws from its surrounding districts.
+            </Typography>
+            <Typography paragraph>
+              Based on our workforce survey, 51% of our respondents mentioned
+              Kathmandu as one of their major tourism-related workplaces.
+              <ul>
+                <li>
+                  Among them, 52% are trekking guides and 31 % are tour guides.
+                  Travel agents and hotel staffs are 7% each.
+                </li>
+                <li>
+                  {' '}
+                  Also from the survey we have found that 16% of the respondents
+                  are from Kathmandu.
+                </li>
+              </ul>
+            </Typography>
+          </>
+        );
+      }
+      case 3: {
+        return (
+          <>
+            <Typography paragraph>
+              What also makes the Nepali tourism industry interesting is the way
+              tourists navigate the country. At present, the country has only
+              one point of entry via air travel -- Tribhuvan International
+              Airport. As a result, incoming international tourists --
+              regardless of their destination -- usually find the need to park
+              themselves at Kathmandu both at the beginning and end of their
+              travels.
+            </Typography>
+          </>
+        );
+      }
+      case 4: {
+        return (
+          <>
+            <Typography paragraph>
+              This map is a standard univariate product that shows district-wise
+              distribution of our respondents, or how reported tourism
+              occupations vary across districts.
+            </Typography>
+            <Typography paragraph>
+              Turns out 20% of our respondents are from Solukhumbu district,
+              where the highest peak of the world, Mount Everest is located.
+              Most of the respondents here are Mountain and trekking guides.
+            </Typography>
+          </>
+        );
+      }
+      default: {
+        return '';
+      }
+    }
+  };
   return (
     <>
       <div className="static-maps-container">
-        {staticMaps.map(map => (
+        {staticMaps.map((map, index) => (
           <Paper
+            key={uid(map, index)}
             elevation={1}
             style={{
               padding: '1rem',
@@ -96,7 +166,7 @@ function StaticMaps() {
               style={{ lineHeight: '1.2', color: 'rgba(0,0,0,0.5)' }}
               gutterBottom
             >
-              {map.description}
+              {getDescription(index + 1)}
             </Typography>
             <ZoomImage src={map.fileName} alt={map.title} />
           </Paper>
